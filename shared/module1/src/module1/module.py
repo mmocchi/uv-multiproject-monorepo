d20aff3bc955161179d98d9ca08bcbf8c6764487
mmocchi <akihiro.matsumoto.exe@gmail.com>
2025-01-11 21:57:45 +0900

    module1を作成

---
+++ shared/module1/src/module1/module.py
@@ -0,0 +1,15 @@
+"""module1のモジュール."""
+
+
+def add(a: int, b: int) -> int:
+    """
+    2つの整数を足し合わせる関数.
+
+    Args:
+        a (int): 整数
+        b (int): 整数
+
+    Returns:
+        int: 2つの整数の和
+    """
+    return a + b
